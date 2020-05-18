a5d8994147da6b8dbea84f5bc8e5ac469eb9bb05
rifat <rifat@hidayahsmart.solutions>
2020-05-18 16:26:42 +0600

    add a function to get some specific message from a line

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
 import xlsxwriter
+import json
+import re
+
+
+def get_message(line):
+    l = re.findall(r"\{[^{}]+\}", line.strip())
+    output_str = None
+    if len(l) > 0:
+        response_dict = json.loads(l[0])
+        output_str = (
+            f"{response_dict['RESPONSECODE']}: {response_dict['RESPONSECONTENT']}"
+        )
+    return output_str
+
 
 request_str = "createRecharge Req"
 response_str = "createRecharge Resp"
@@ -22,19 +36,21 @@ for msg_id in ids:
                 exec_str = line.split()[3]
                 demand_str = exec_str.replace("[", "").replace("]", "")
                 id_c = "A" + str(i)
-                d_c = "B" + str(j)
+                d_c = "E" + str(j)
                 worksheet.write(id_c, msg_id)
                 worksheet.write(d_c, line)
                 for line_2 in f:
                     if demand_str in line_2 and response_str in line_2:
                         j = j + 1
-                        d_c = "B" + str(j)
+                        d_c = "E" + str(j)
                         worksheet.write(d_c, line_2)
+                        worksheet.write("B" + str(j), get_message(line_2))
                         break
                     elif demand_str in line_2 and power_gateway in line_2:
                         j = j + 1
-                        d_c = "B" + str(j)
+                        d_c = "E" + str(j)
                         worksheet.write(d_c, line_2)
+                        worksheet.write("B" + str(j), get_message(line_2))
                         break
                 i = i + 2
                 j = j + 1
