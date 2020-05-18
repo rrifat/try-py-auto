998af409c780d05b93b77d85a27282d4150ec8b2
rifat <rifat@hidayahsmart.solutions>
2020-05-18 17:18:11 +0600

    comment an unnecessary line to fix a bug

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ def get_message(line):
     output_str = None
     if len(l) > 0:
         response_dict = json.loads(l[0])
-        print(response_dict)
         if "RESPONSECODE" in response_dict and "RESPONSECONTENT" in response_dict:
             output_str = (
                 f"{response_dict['RESPONSECODE']}: {response_dict['RESPONSECONTENT']}"
@@ -36,19 +35,19 @@ for msg_id in ids:
         for line in f:
             if msg_id in line and request_str in line:
                 exec_str = line.split()[3]
-                demand_str = exec_str.replace("[", "").replace("]", "")
+                # demand_str = exec_str.replace("[", "").replace("]", "")
                 id_c = "A" + str(i)
                 d_c = "E" + str(j)
                 worksheet.write(id_c, msg_id)
                 worksheet.write(d_c, line)
                 for line_2 in f:
-                    if demand_str in line_2 and response_str in line_2:
+                    if exec_str in line_2 and response_str in line_2:
                         j = j + 1
                         d_c = "E" + str(j)
                         worksheet.write(d_c, line_2)
                         worksheet.write("B" + str(j), get_message(line_2))
                         break
-                    elif demand_str in line_2 and power_gateway in line_2:
+                    elif exec_str in line_2 and power_gateway in line_2:
                         j = j + 1
                         d_c = "E" + str(j)
                         worksheet.write(d_c, line_2)
