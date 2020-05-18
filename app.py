9f99b55854ab1838a6134572035c1e31a94603bc
rifat <rifat@hidayahsmart.solutions>
2020-05-18 23:37:22 +0600

    refactor a bit

--- app.py
+++ app.py
@@ -2,8 +2,15 @@ import xlsxwriter
 import json
 import re
 
+request_str = "createRecharge Req"
+response_str = "createRecharge Resp"
+power_gateway = "power-gateway provider"
 
-# Helper functions
+log_file = "files/catalina.2020-04-13.out"
+id_file = "files/ids.txt"
+output_file = "demo.xlsx"
+
+# Helpers
 def get_message(line):
     l = re.findall(r"\{[^{}]+\}", line.strip())
     output_str = None
@@ -16,45 +23,48 @@ def get_message(line):
     return output_str
 
 
-# Main
-request_str = "createRecharge Req"
-response_str = "createRecharge Resp"
-power_gateway = "power-gateway provider"
+def get_responses(exec_str, file):
+    for line in file:
+        if exec_str in line and response_str in line:
+            return [line, get_message(line)]
+        if exec_str in line and power_gateway in line:
+            return [line, get_message(line)]
 
-workbook = xlsxwriter.Workbook("demo.xlsx")
+
+def get_ids():
+    ids = []
+    with open(id_file, "r") as f:
+        for line in f:
+            ids.append(line.strip())
+    return ids
+
+
+# Main
+ids = get_ids()
+
+workbook = xlsxwriter.Workbook(output_file)
 worksheet = workbook.add_worksheet()
 
-ids = []
-with open("files/ids.txt") as f_n:
-    for line in f_n.readlines():
-        ids.append(line.strip())
-
-i = 1
-j = 1
+id_row = 1
 
 for msg_id in ids:
-    with open("files/catalina.2020-04-13.out", "r") as f:
+    with open(log_file, "r") as f:
         for line in f:
             if msg_id in line and request_str in line:
                 exec_str = line.split()[3]
-                # demand_str = exec_str.replace("[", "").replace("]", "")
-                id_c = "A" + str(i)
-                d_c = "E" + str(j)
-                worksheet.write(id_c, msg_id)
-                worksheet.write(d_c, line)
-                for line_2 in f:
-                    if exec_str in line_2 and response_str in line_2:
-                        j = j + 1
-                        d_c = "E" + str(j)
-                        worksheet.write(d_c, line_2)
-                        worksheet.write("B" + str(j), get_message(line_2))
-                        break
-                    elif exec_str in line_2 and power_gateway in line_2:
-                        j = j + 1
-                        d_c = "E" + str(j)
-                        worksheet.write(d_c, line_2)
-                        worksheet.write("B" + str(j), get_message(line_2))
-                        break
-                i = i + 2
-                j = j + 1
+
+                worksheet.write("A" + str(id_row), msg_id)
+                worksheet.write("E" + str(id_row), line)
+
+                responses = get_responses(exec_str, f)
+                response_line = responses[0]
+                response_message = responses[1]
+
+                response_row = id_row + 1
+
+                worksheet.write("E" + str(response_row), response_line)
+                worksheet.write("B" + str(response_row), response_message)
+
+                id_row += 2
+
 workbook.close()
