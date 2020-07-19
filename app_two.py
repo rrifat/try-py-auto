a146d6c592dbbb2bee7c388f0e2b7bc00b8e62ef
rifat <rifat@hidayahsmart.solutions>
2020-07-19 17:32:38 +0600

    add new script

---
+++ app_two.py
@@ -0,0 +1,19 @@
+from openpyxl import Workbook, load_workbook
+
+file = "files/token_import.xlsx"
+workbook = load_workbook(file)
+
+meters = []
+sheet = workbook.active
+
+for i in range(1, len(sheet["A"])):
+    meter_no = sheet["A"][i]
+    if meter_no.value in meters:
+        ptr += 1
+        sheet[f"C{i + 1}"].value = ptr
+    else:
+        ptr = 1
+        sheet[f"C{i + 1}"].value = ptr
+        meters.append(meter_no.value)
+
+workbook.save(file)
